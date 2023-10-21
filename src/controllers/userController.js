822d96a50467f31911087557e212734de1840475
Saquib <saquibmohammad38@gmail.com>
2023-10-21 23:23:03 +0530

    complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,39 +1,29 @@
-const userModel=require('../models/userModel')
+const userModel = require('../models/userModel');
 
-const userRegistration= async function(req,res){
-
-try{
-const data=req.body
-const createUser=await userModel.create(data)
-res.status(201).send({status:true,messege:"user registration sucssefully",data:createUser})
-}
-catch(err){
-    res.status(500).send({status:false,message:err.message})
-}
-}
-
-const getUser=async function(req,res){
-
-    try{
-       
-
-        let id = req.query.id;
-if(id){
-        const fetchUser=await userModel.find({_id:id})
-        console.log(fetchUser)
-        res.status(200).send({status:true,messege:fetchUser})
-        if (!id) {
-            const getUser=await userModel.find()
-            res.status(200).send({status:true,messege:getUser})
-        }
-
-      
-
-    }}
-    catch(err){
-        res.status(500).send({status:false,message:err.message})
+module.exports = {
+  userRegistration: async function(req, res) {
+    try {
+      const data = req.body;
+      const createUser = await userModel.create(data);
+      res.status(201).send({ status: true, message: "user registration successfully", data: createUser });
+    } catch (err) {
+      res.status(500).send({ status: false, message: err.message });
     }
-}
+  },
 
-
-module.exports={userRegistration,getUser}
+  getUser: async function(req, res) {
+    try {
+      let id = req.query.id;
+      if (id) {
+        const fetchUser = await userModel.find({ _id: id });
+        console.log(fetchUser);
+        res.status(200).send({ status: true, message: fetchUser });
+      } else {
+        const getUsers = await userModel.find();
+        res.status(200).send({ status: true, message: getUsers });
+      }
+    } catch (err) {
+      res.status(500).send({ status: false, message: err.message });
+    }
+  }
+};
